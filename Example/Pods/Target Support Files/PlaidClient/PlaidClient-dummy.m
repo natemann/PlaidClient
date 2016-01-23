389b7a396e868582f1c504f050c6944fd2055ccd
Nate Mann <nathan.mann@me.com>
2016-01-23 15:30:11 -0600

    CocoaPod Initial Commit

---
+++ Example/Pods/Target Support Files/PlaidClient/PlaidClient-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_PlaidClient : NSObject
+@end
+@implementation PodsDummy_PlaidClient
+@end
---
+++ Example/Pods/Target Support Files/Pods-PlaidClient_Tests/Pods-PlaidClient_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PlaidClient_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_PlaidClient_Tests
+@end
