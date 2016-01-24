b1aa3dfdfd3cadd59c9ad959f85b3dd1c4d9840c
Nate Mann <nathan.mann@me.com>
2016-01-23 22:40:44 -0600

    added alamofire

--- Pods/Target Support Files/Pods-PlaidClientTests/Pods-PlaidClientTests-frameworks.sh
+++ Pods/Target Support Files/Pods-PlaidClientTests/Pods-PlaidClientTests-frameworks.sh
@@ -84,10 +84,8 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "Pods-PlaidClientTests/AFNetworking.framework"
   install_framework "Pods-PlaidClientTests/Alamofire.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "Pods-PlaidClientTests/AFNetworking.framework"
   install_framework "Pods-PlaidClientTests/Alamofire.framework"
 fi
